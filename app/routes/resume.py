18620fd52913e9449a6e0e9f5a303314132f3d72
thanks-to-visit <gleuser83@gmail.com>
2026-09-24 00:36:34 +0530

    Update resume.py async added

--- app/routes/resume.py
+++ app/routes/resume.py
@@ -7,7 +7,7 @@ from app.services.resume_service import generate_resume
 router = APIRouter(prefix="/resume",tags=["Resume"])
 
 @router.post("/generate")
-def create_resume(data:ResumeRequest) :
+async def create_resume(data:ResumeRequest) :
   pdf_path = generate_resume(data)
   return FileResponse(
     path=pdf_path,
