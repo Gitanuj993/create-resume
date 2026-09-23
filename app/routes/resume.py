0e1434666eb52773140e05bc22e0e1d42b517d2f
thanks-to-visit <gleuser83@gmail.com>
2026-09-24 00:33:04 +0530

    Update resume.py renamed function

--- app/routes/resume.py
+++ app/routes/resume.py
@@ -7,7 +7,7 @@ from app.services.resume_service import generate_resume
 router = APIRouter(prefix="/resume",tags=["Resume"])
 
 @router.post("/generate")
-def generate_resume(data:ResumeRequest) :
+def create_resume(data:ResumeRequest) :
   pdf_path = generate_resume(data)
   return FileResponse(
     path=pdf_path,
